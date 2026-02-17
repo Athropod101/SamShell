bcad87343fe288730a66286603b265b47dff7ab2
Gabriel Diaz <gdiaz372003@proton.me>
2026-02-17 03:20:02 -0400

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,28 +2,70 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+int cap = 1024;
 
-  while (true) {
-	  printf("$ ");
+struct ShellInput {
+	char *cmd;
+	char *prompt;
+	bool hasPrompt;
+	size_t cmdEnd;
+	char full[];
+};
 
-	  /* Get the user's command */
-	  int cap = 1024;
-	  char command[cap];
-	  fgets(command, cap, stdin);
-
-	  /* Trim the Newline */
-	  command[strcspn(command, "\n")] = '\0';
-
-	  /* Building the exit command */
-	  if (strcmp(command, "exit") == 0) {
-		  break;
-	  }
-
-	  /* Print the command result */
-	  printf("%s: command not found\n", command);
-  }
-  return 0;
+void FetchCMD(struct ShellInput *input) {
+	input->cmdEnd = strcspn(input->full, " ");
+	if (input->cmdEnd == strlen(input->full)) {
+		input->full[input->cmdEnd - 1] = '\0';
+		input->hasPrompt = false;
+	}
+	else {
+		input->full[input->cmdEnd] = '\0';
+		input->hasPrompt = true;
+	}
+	input->cmd = &input->full[0];
 }
+
+void FetchPrompt(struct ShellInput *input) {
+	input->prompt = &input->full[input->cmdEnd + 1];
+}
+
+void Parse_input(struct ShellInput *input) {
+	FetchCMD(input);
+	if (input->hasPrompt) {
+		FetchPrompt(input);
+	}
+}
+
+int main(int argc, char *argv[]) {
+
+	// Flush after every printf
+	setbuf(stdout, NULL);
+	struct ShellInput *input = malloc(sizeof(struct ShellInput) + cap);
+
+	while (true) {
+		printf("$ ");
+
+		/* Get the user's input */
+		fgets(input->full, cap, stdin);
+		Parse_input(input);
+
+		/* Building the exit input */
+		if (strcmp(input->cmd, "exit") == 0) {
+			break;
+		}
+		/* Building the echo input */
+		else if (strcmp(input->cmd, "echo") == 0) {
+			if (input->hasPrompt) {
+				printf("%s", input->prompt);
+			}
+		}
+		/* Returning Null input Error */
+		else {
+		printf("%s: command not found\n", input->cmd);
+		}
+	}
+
+	free(input);
+	return 0;
+}
+
